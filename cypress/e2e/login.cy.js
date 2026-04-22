e5795475a07646699c1eaadd07aa20827996ddc0
Gozde Arici <gozdenurarici@gmail.com>
2026-04-22 16:01:18 +0300

    fix fixtures mock data in login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,26 @@
 import LoginPage from "../pages/LoginPage"
 
 describe('Login Test', () => {
-    it('should login successfully with invalid credentials', () => {
-        cy.fixture('users').then(({ valid }) => {
-            LoginPage.login(valid.username, valid.password)
+    let validUser, invalidUser
+
+    beforeEach('stub external tracking requests', () => {
+        cy.stubExternalServices()
+        cy.fixture('users').then(data => {
+            validUser = data.valid
+            invalidUser = data.invalid
         })
+    })
+
+    it('should login successfully with invalid credentials', () => {
+        LoginPage.login(invalidUser.username, invalidUser.password)
 
         LoginPage.errorMessage()
-            .should('be.visible')
+            .should('be.visible', { timeout: 10000 })
             .and('contain.text', 'Username and password do not match')
     })
 
     it('should login successfully with valid credentials', () => {
-        cy.fixture('users').then(({ invalid }) => {
-            LoginPage.login(invalid.username, invalid.password)
-        })
+        LoginPage.login(validUser.username, validUser.password)
 
         cy.url().should('include', '/inventory')
     })
